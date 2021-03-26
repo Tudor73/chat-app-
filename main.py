927a6c4064d4ca00f35cb14d81dd7c413851c12e
Tudor73 <tudorminciunescu@gmail.com>
2021-03-27 01:53:13 +0200

    started working on the web app

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template, request, url_for, redirect
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "<h1>HIIIII</h1>"
+    return render_template("base.html")
 
+@app.route('/login', methods = ["POST", "GET"])
+def login():
+    if request.method == "POST":
+        user = request.form["name"]
+        return redirect(url_for("user", user = user))
+    else:
+        return render_template("login.html")
+
+@app.route("/<user>")
+def user(user):
+    return f"<h1>{user}</h1>"
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug = True)
